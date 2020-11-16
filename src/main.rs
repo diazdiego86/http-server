f7130d94582747a37a5cebf9a83e877f426a2bcd
Diego Diaz <diazdiego86@gmail.com>
2020-11-16 21:13:00 +0100

    dummy http server (mod structure)

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use http::Request;
+use http::Method;
+use server::Server;
+
+mod server;
+mod http;
+
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+
+    server.run();
+}
