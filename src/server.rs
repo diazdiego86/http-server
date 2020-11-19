2810128acbf9582860dc9a16ce5d6a8ae4e3a59c
Diego Diaz <diazdiego86@gmail.com>
2020-11-19 22:17:14 +0100

    accepting tcp connections (only a buffer of 1024 for this dummy impl)

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 pub struct Server {
     addr: String,
 }
@@ -12,5 +13,20 @@ impl Server {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buf = [0; 1024];
+                    match stream.read(&mut buf) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buf));
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to established a connection: {}", e),
+            }
+        }
     }
 }
